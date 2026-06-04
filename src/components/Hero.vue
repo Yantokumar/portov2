0175112bc288e6fb83e7ed4e21114781ebd4eec9
Yantokumar <rudih2205n@gmail.com>
2026-06-04 14:54:53 +0700

    feat: sukses setup tailwind v4 dan vue components

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -2,14 +2,19 @@
   <section class="py-20 text-center md:text-left flex flex-col md:flex-row items-center justify-between gap-8">
     <div class="space-y-4 max-w-2xl">
       <h1 class="text-4xl md:text-6xl font-extrabold tracking-tight">
-        Halo, Saya <span class="text-emerald-400">Bahrudin</span>
+        Halo, Saya <span class="text-emerald-400">Bahrudin Yusup Caruban</span>
       </h1>
       <p class="text-xl text-slate-400">
         Mahasiswa Sistem Informasi & Pembelajar Full-Stack Web Development.
       </p>
     </div>
-    <div class="w-48 h-48 rounded-full bg-slate-800 border-2 border-emerald-500 flex items-center justify-center text-slate-500">
-      [ Foto Profil ]
+    
+    <div class="w-48 h-48 rounded-full bg-slate-800 border-2 border-emerald-500 overflow-hidden flex items-center justify-center">
+      <img 
+        src="../assets/hero.png" 
+        alt="Foto Profil Bahrudin" 
+        class="w-full h-full object-cover"
+      />
     </div>
   </section>
 </template>
